155645c7eef67f60219d84a02f6937ccce72194f
thedrean <at4344a@student.american.edu>
2014-04-05 12:45:45 -0400

    did a bunch of stuff, pals

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
 
 import flask
+try:
+	import bloomfuncs
+except ImportError:
+	pass
 
 app = flask.Flask(__name__)
 
@@ -10,6 +14,14 @@ def root():
 @app.route("/saywordisdufhisudhfisudhfiu/<word>")
 def sayword(word):
 	return "You said "+word
+
+@app.route("/query/<ticker>")
+def query(ticker):
+	# This is where we will access the bloomberg API
+	try:
+		return bloomfuncs.main(ticker)
+	except Exception:
+		return flask.jsonify(ticker=ticker, info="$MONAYYYY", news="You rich boy!")
 
 if __name__ == "__main__":
 	app.run(debug=True)
--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,30 @@
 
 $(function() {
 	$( "#tags" ).autocomplete({source: availableTags});
+	// var stuff = [
+	// ["TWTR", "$1", "poopoo"],
+	// ["IUIBIUBIB", "$23", "IUHI"],
+	// ["JKHKJH", "$56", "UGY"]]
+	// stuff.forEach(function(e){
+	// 	newtablerow(e[0], e[1], e[2])
+	// })
+	$("#tags").keydown(function(e){
+		if (e.which == 13){
+			var inputval = $("#tags").val()
+			if (availableTags.indexOf(inputval) == -1){
+				alert("Invalid value.  Please use the autocomplete feature\n")
+				return
+			}
+			var ticker = inputval.split(" ")[0]
+			$.ajax({
+				url:"/query/"+ticker
+			}).done(newtablerow)
+		}
+	})
 });
+
+function newtablerow(obj){
+	var compiled = _.template($("#tablerow").html(), obj)
+	$("#stocktable").html($("#stocktable").html()+compiled)
+
+}
