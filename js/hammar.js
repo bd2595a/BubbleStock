50fe5f24aa5095d5bd24e2a9db13898f1b793b15
Dri Torres <dri.nicole.torres@gmail.com>
2014-04-05 09:35:10 -0400

    Create hammar.js

---
+++ js/hammar.js
@@ -0,0 +1,8 @@
+/*! Hammer.JS - v1.0.9 - 2014-03-18
+ * http://eightmedia.github.com/hammer.js
+ *
+ * Copyright (c) 2014 Jorik Tangelder <j.tangelder@gmail.com>;
+ * Licensed under the MIT license */
+
+
+!function(a,b){"use strict";function c(){d.R
